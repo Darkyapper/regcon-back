830d26c1ee364c8582a3a1a31c76b80a4ce0359b
David Chay <yapperdark2@gmail.com>
2025-02-10 10:25:37 -0600

    add fs module import to server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken'); 
 require('dotenv').config();
 const axios = require('axios');
+const fs = require('fs');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
